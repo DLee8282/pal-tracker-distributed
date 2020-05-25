3af11e246611af3f6c5e51a5924924643f7157d9
DLee8282 <DavidLee8282@gmail.com>
2020-05-24 22:44:12 -0600

    Cleaned up some code

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -12,17 +12,17 @@ public class ProjectClient {
 
 
     private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
+    private final String endpoint;
     private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.endpoint = registrationServerEndpoint;
     }
     @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        ProjectInfo project = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
         projectsCache.put(projectId,project);
         return project;
     }
